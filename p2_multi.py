7baa5b0e57021fcfe29a0722605848e95aa0f65b
seigo07 <seven_8s@icloud.com>
2023-03-19 01:25:16 +0000

    multi part

--- p2_multi.py
+++ p2_multi.py
@@ -1,4 +1,5 @@
-from p2_binary_functions import *
+
+from p2_multi_functions import *
 from sklearn.model_selection import train_test_split
 from imblearn.under_sampling import RandomUnderSampler
 from sklearn.metrics import confusion_matrix
@@ -6,6 +7,7 @@ import seaborn as sns
 import numpy as np
 from sklearn.metrics import classification_report
 from sklearn.metrics import balanced_accuracy_score
+
 
 # 1. Loading the data
 
@@ -24,8 +26,7 @@ plot_target_frequency(data_dict)
 # Splitting the training dataset in order to obtain a mock test set,
 # due to no target labels for the X_test.csv file.
 
-X_train, X_val, y_train, y_val = train_test_split(data_dict['X_train'], data_dict['y_train'], test_size=0.2,
-                                                  random_state=23, shuffle=True, stratify=data_dict['y_train'])
+X_train, X_val, y_train, y_val = train_test_split(data_dict['X_train'], data_dict['y_train'], test_size=0.2, random_state=23, shuffle=True, stratify=data_dict['y_train'])
 
 # Getting the undersampled data for training without undersampling the mock testing set
 # in order to keep the distribution of the classes close to the distribution of the original dataset.
@@ -59,7 +60,7 @@ y_pred = knn_cf.predict(X_val)
 # The confusion matrix for KNN which will tell us how each class was misclassified.
 
 cf = confusion_matrix(y_val, y_pred)
-sns.heatmap(cf / np.sum(cf), annot=True, fmt='.2%', cmap='Blues')
+sns.heatmap(cf/np.sum(cf), annot=True, fmt='.2%', cmap='Blues')
 
 # The classification report for KNN which will give us detailed evaluation metrics.
 
@@ -70,7 +71,7 @@ print('Balanced accuracy: ', balanced_accuracy_score(y_val, y_pred))
 
 # Training KNN with undersampled dataset.
 
-knn_cf_under = kn_cross_validate_pca(X_train_under, y_train_under, scorer=scorer)
+knn_cf_under = kn_cross_validate_pca(X_train_under, y_train_under, scorer)
 
 # Making predictions on evaluation set with KNN trained on the undersampled dataset
 # by Using the KNN classifier trained using the undersampled dataset.
@@ -80,7 +81,7 @@ y_pred_under = knn_cf_under.predict(X_val)
 # The confusion matrix which will tell us how each class was misclassified.
 
 cf_under = confusion_matrix(y_val, y_pred_under)
-sns.heatmap(cf_under / np.sum(cf_under), annot=True, fmt='.2%', cmap='Blues')
+sns.heatmap(cf_under/np.sum(cf_under), annot=True, fmt='.2%', cmap='Blues')
 
 # The classification report for KNN which will give us detailed evaluation metrics.
 
@@ -89,7 +90,7 @@ print(cr_under)
 
 # 5. Selecting and training a classification model,
 
-rf_cf = rf_cross_validate_pca(X_train, y_train, scorer=scorer)
+rf_cf = rf_cross_validate_pca(X_train, y_train, scorer)
 
 # Making predictions on the validation set with RF by using the RF classifier
 # to make predictions on the validation set to allow us to evaluate its performance.
@@ -110,7 +111,7 @@ print('Balanced accuracy: ', balanced_accuracy_score(y_val, y_pred))
 
 # Training RF with undersampled dataset using the undersampled dataset.
 
-rf_cf_under = rf_cross_validate_pca(X_train_under, y_train_under, scorer=scorer)
+rf_cf_under = rf_cross_validate_pca(X_train_under, y_train_under, scorer)
 
 # Making prediction on evaluation set with RF using the RF classifier trained using the undersampled dataset
 # to make predictions on the validation set to allow us to evaluate its performance.
@@ -129,7 +130,7 @@ print(cr_under)
 
 # 6. Selecting and training another classification model
 
-svc_cf = svc_cross_validate_pca(X_train, y_train, scorer=scorer)
+svc_cf = svc_cross_validate_pca(X_train, y_train, scorer)
 
 # Making predictions on the validation set with SVC using the SVC classifier
 # to make predictions on the validation set to allow  us to evaluate its performance.
@@ -146,7 +147,7 @@ print('Balanced accuracy: ', balanced_accuracy_score(y_val, y_pred))
 
 # Training SVC with undersampled dataset
 
-svc_cf_under = svc_cross_validate_pca(X_train_under, y_train_under, scorer=scorer)
+svc_cf_under = svc_cross_validate_pca(X_train_under, y_train_under, scorer)
 
 # Making prediction on evaluation set with SVC under using the SVC classifier trained using the undersampled dataset
 # to make predictions on the validation set to allow  us to evaluate its performance.
@@ -166,9 +167,23 @@ print(cr_under)
 # Producing the Y_test.csv file which is the file that will be used to evaluate the final performance of the classifier.
 # Decided that will be used for the SVM classifier that was trained on the original dataset after analysing the results.
 
-# X_test = data_dict['X_test']
-# y_test = svc_cf.predict(X_test)
+X_test = data_dict['X_test']
+y_test = svc_cf.predict(X_test)
+print(len(y_test))
 # np.savetxt("output/Y_test.csv", y_test, fmt="%s")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- p2_multi_functions.py
+++ p2_multi_functions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 import time
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
@@ -12,14 +13,14 @@ from sklearn.svm import LinearSVC
 
 # Loading the binary data
 def load_data():
-    # print('Loading binary class data in note...')
-    X_train_df = pd.read_csv("binary/X_train.csv", header=None)
+    # print('Loading multi-class data...')
+    X_train_df = pd.read_csv("multi/X_train.csv", header=None)
     # X_train_df.info()
 
-    y_train_df = pd.read_csv("binary/Y_train.csv", header=None)
+    y_train_df = pd.read_csv("multi/Y_train.csv", header=None)
     # print('Unique values', y_train_df.iloc[:, 0].unique())
 
-    X_test_df = pd.read_csv("binary/X_test.csv", header=None)
+    X_test_df = pd.read_csv("multi/X_test.csv", header=None)
     # X_test_df.info()
 
     data_dict = {
@@ -34,26 +35,24 @@ def load_data():
 
 # Cleaning the data by removing the features taken from a random normal distribution
 def clean_data(data_dict):
-    # print('Cleaning data...')
+    print('Cleaning data...')
     X_train_df = data_dict['X_train']
     X_test_df = data_dict['X_test']
     y_train_df = data_dict['y_train']
 
-    # df.columns is zero-based pd.Index
-    X_train_df = X_train_df.drop(X_train_df.columns[900:916], axis=1)
-    # df.columns is zero-based pd.Index
-    X_test_df = X_test_df.drop(X_test_df.columns[900:916], axis=1)
+    X_train_df = X_train_df.drop(X_train_df.columns[900:916], axis=1)  # df.columns is zero-based pd.Index
+    X_test_df = X_test_df.drop(X_test_df.columns[900:916], axis=1)  # df.columns is zero-based pd.Index
 
-    # print('X train after cleaning:')
+    print('X train after cleaning:')
     X_train_df.info()
-    # print('X_test after cleaning')
+    print('X_test after cleaning')
     X_test_df.info()
     data_dict = {
         'X_train': X_train_df,
         'X_test': X_test_df,
         'y_train': y_train_df
     }
-    # print('\n')
+    print('\n')
     return data_dict
 
 
@@ -61,9 +60,9 @@ def clean_data(data_dict):
 def plot_target_frequency(data_dict):
     y_train = data_dict['y_train']
     y_train.columns = ['label']
-    # print(y_train.columns)
+    print(y_train.columns)
     total = float(len(y_train))
-    # print('total', total)
+    print('total', total)
     plot = sns.countplot(x='label', data=y_train)
     for p in plot.patches:
         height = p.get_height()
@@ -71,7 +70,7 @@ def plot_target_frequency(data_dict):
                   height + 3,
                   '{:1.3f}'.format(height / total),
                   ha="center")
-    # plt.show()
+    plt.show()
 
 
 # Defining the estimator as well as the grid search for training KNN.
@@ -87,35 +86,38 @@ def kn_cross_validate_pca(X_train, y_train, scorer):
         'cf__p': [2]
     }
 
-    # print('Grid: ', params)
-    # print('Scorer: ', scorer)
+    print('Grid: ', params)
+
+    print('Scorer: ', scorer)
 
     cf = GridSearchCV(pipeline, params, cv=kf, n_jobs=-1, scoring=scorer, refit=scorer[0])
 
     start = time.time()
     cf.fit(X_train, y_train)
     end = time.time()
+    print('K-nearest neighbors cross-val time elapsed: ', end - start)
 
-    # print('K-nearest neighbors cross-val time elapsed: ', end - start)
-    # print('Best params: ', cf.best_params_)
-    # print('PCA number of components', cf.best_estimator_.named_steps['pca'].n_components_)
+    print('Best params: ', cf.best_params_)
+
+    print('PCA number of components', cf.best_estimator_.named_steps['pca'].n_components_)
 
     balanced_acc_score = cf.best_score_ * 100
 
     acc_score = cf.cv_results_['mean_test_accuracy'][cf.best_index_] * 100
 
-    # print("Best cross-val balanced accuracy score: " + str(round(balanced_acc_score, 2)) + '%')
-    # print("Best cross-val accuracy score: " + str(round(acc_score, 2)) + '%')
+    print("Best cross-val balanced accuracy score: " + str(round(balanced_acc_score, 2)) + '%')
+    print("Best cross-val accuracy score: " + str(round(acc_score, 2)) + '%')
 
     cv_results = pd.DataFrame(cf.cv_results_)
     # display(cv_results)
 
-    # print('\n')
+    print('\n')
     return cf.best_estimator_
 
 
 # Defining the estimator as well as the grid search for training Random Forest Classifier.
-# The estimator is a pipeline in which the features are scaled.
+# The estimator is a pipeline in which the features are scaled,
+# PCA is performed and a Random Forest Classifier classifier is trained.
 def rf_cross_validate_pca(X_train, y_train, scorer):
     kf = KFold(n_splits=5, shuffle=True, random_state=42)
     pipeline = Pipeline(
@@ -128,34 +130,35 @@ def rf_cross_validate_pca(X_train, y_train, scorer):
         'cf__min_samples_split': [2],
     }
 
-    # print('Grid: ', params)
-    # print('Scorer: ', scorer)
+    print('Grid: ', params)
+
+    print('Scorer: ', scorer)
 
     cf = GridSearchCV(pipeline, params, cv=kf, n_jobs=-1, scoring=scorer, refit=scorer[0])
 
     start = time.time()
     cf.fit(X_train, y_train)
     end = time.time()
+    print('Random Forest cross-val time elapsed: ', end - start)
 
-    # print('Random Forest cross-val time elapsed: ', end - start)
-    # print('Best params: ', cf.best_params_)
-    # print('PCA number of components', cf.best_estimator_.named_steps['pca'].n_components_)
+    print('Best params: ', cf.best_params_)
+
+    print('PCA number of components', cf.best_estimator_.named_steps['pca'].n_components_)
 
     balanced_acc_score = cf.best_score_ * 100
 
     acc_score = cf.cv_results_['mean_test_accuracy'][cf.best_index_] * 100
 
-    # print("Best cross-val balanced accuracy score: " + str(round(balanced_acc_score, 2)) + '%')
-    # print("Best cross-val accuracy score: " + str(round(acc_score, 2)) + '%')
+    print("Best cross-val balanced accuracy score: " + str(round(balanced_acc_score, 2)) + '%')
+    print("Best cross-val accuracy score: " + str(round(acc_score, 2)) + '%')
 
     cv_results = pd.DataFrame(cf.cv_results_)
     # display(cv_results)
 
-    # print('\n')
+    print('\n')
     return cf.best_estimator_
 
 
-# Defining the estimator as well as the grid search. The estimator is a pipeline in which the features are scaled.
 def svc_cross_validate_pca(X_train, y_train, scorer):
     kf = KFold(n_splits=5, shuffle=True, random_state=42)
     pipeline = Pipeline(
@@ -167,29 +170,30 @@ def svc_cross_validate_pca(X_train, y_train, scorer):
         'cf__dual': [False],
     }
 
-    # print('Grid: ', params)
-    # print('Scorer: ', scorer)
+    print('Grid: ', params)
+
+    print('Scorer: ', scorer)
 
     cf = GridSearchCV(pipeline, params, cv=kf, n_jobs=-1, scoring=scorer, refit=scorer[0])
 
     start = time.time()
     cf.fit(X_train, y_train)
     end = time.time()
+    print('SVC cross-val time elapsed: ', end - start)
 
-    # print('SVC cross-val time elapsed: ', end - start)
-    # print('Best params: ', cf.best_params_)
-    # print('PCA number of components', cf.best_estimator_.named_steps['pca'].n_components_)
+    print('Best params: ', cf.best_params_)
+
+    print('PCA number of components', cf.best_estimator_.named_steps['pca'].n_components_)
 
     balanced_acc_score = cf.best_score_ * 100
+
     acc_score = cf.cv_results_['mean_test_accuracy'][cf.best_index_] * 100
 
-    # print("Best cross-val balanced accuracy score: " + str(round(balanced_acc_score, 2)) + '%')
-    # print("Best cross-val accuracy score: " + str(round(acc_score, 2)) + '%')
+    print("Best cross-val balanced accuracy score: " + str(round(balanced_acc_score, 2)) + '%')
+    print("Best cross-val accuracy score: " + str(round(acc_score, 2)) + '%')
 
     cv_results = pd.DataFrame(cf.cv_results_)
     # display(cv_results)
 
-    # print('\n')
+    print('\n')
     return cf.best_estimator_
-
-
